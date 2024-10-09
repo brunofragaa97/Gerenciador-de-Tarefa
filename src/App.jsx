e318e55858c2903ccc0ef6d9246eb27ca32e8877
Bruno de Fraga <37921531+brunofragaa97@users.noreply.github.com>
2024-10-09 11:35:39 -0300

    versão 0.0.6
    Corrigido bug de tela branca ao adcionar muitas tarefas, o problema estava na div pai do container no aplicativo princial (App.jsx). Adcionado ''overflow-y-auto"
    para rolagem ao adcionar muitas tarefas.

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,8 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-blue-950 flex justify-center p-8">
-      <div className="space-y-4 w-[500px]">
+    <div className="w-screen min-h-screen bg-blue-950 flex justify-center p-8">
+      <div className="w-[500px] space-y-4 ">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,7 +3,8 @@ import { Trash2 } from "lucide-react";
 
 function Tasks(props) {
   return (
-    <ul className="space-y-4 p-6 bg-slate-100 rounded-md shadow">
+    // No componente Tasks
+    <ul className="space-y-4 p-6 bg-slate-100 rounded-md shadow max-h-[400px] overflow-y-auto">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
